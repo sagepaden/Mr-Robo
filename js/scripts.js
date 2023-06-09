134886d4555d5e2708cbe778a7b1a6dbce3eafab
Sage Paden <sage.paden@gmail.com>
2023-06-09 11:45:40 -0700

    test for 2 being replaced has passed

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ function mrRobo(userNumber) {
   for (i = 0; i <= userNumber; i++) {
     if (i.toString().includes('3')) {
       loopOutput.push("Won't you be my neighbor?");
+    } else if (i.toString().includes('2')) {
+      loopOutput.push("Boop!");
+    
     } else {
       loopOutput.push(i);
     }
