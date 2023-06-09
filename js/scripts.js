ac04dd3caf76d03a950a431c4ba98213c74d8d49
Sage Paden <sage.paden@gmail.com>
2023-06-09 14:01:56 -0700

    misspelled the id for the ui-array-output in my HTML. everything works as intended now

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function mrRobo(number) {
   let loopOutput = [];
 
   for (i = 0; i <= number; i++) {
-    if (i.toString().includes('3')) {
+    if (i.toString().includes("3")) {
       loopOutput.push("Won't you be my neighbor?");
 
-    } else if (i.toString().includes('2')) {
+    } else if (i.toString().includes("2")) {
       loopOutput.push("Boop!");
 
-    } else if (i.toString().includes('1')) {
+    } else if (i.toString().includes("1")) {
       loopOutput.push("Beep!");
     
     } else {
@@ -25,9 +25,8 @@ function mrRobo(number) {
 
 document.addEventListener('submit', function(event) {
   event.preventDefault();
-  const usersNumber = document.getElementById('ui-user-number').value;
-  const arrayOutput = mrRobo(usersNumber);
+  const userNumber = document.getElementById("ui-user-number").value;
+  const arrayOutput = mrRobo(userNumber);
   const outputAsString = arrayOutput.join(", ");
   document.getElementById("ui-array-output").innerText = "Your cheeky lil array is:" + outputAsString;
-
-})
+});
